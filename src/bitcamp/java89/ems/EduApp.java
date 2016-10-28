99199795a39132585b50ec268536693521af574a
Jongwoo.dev <Jongwoo.dev@gmail.com>
2016-10-28 11:27:06 +0900

    ex5

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -7,7 +7,7 @@ public class EduApp {
 
   public static void main(String[] args) {
     //EduApp에서 사용하는 keyScan을 StudentController와 공유한다.
-    TeacherController.keyScan = keyScan;
+    TeacherController teacherController = new TeacherController(keyScan);
 
     System.out.println("비트캠프 관리시스템에 오신걸 환영합니다.");
 
@@ -17,9 +17,9 @@ public class EduApp {
       String cmd = keyScan.nextLine().toLowerCase();
 
       switch (cmd) {
-      case "add": TeacherController.doAdd(); break;
-      case "list": TeacherController.doList(); break;
-      case "view": TeacherController.doView(); break;
+      case "add": teacherController.doAdd(); break;
+      case "list": teacherController.doList(); break;
+      case "view": teacherController.doView(); break;
       case "quit": System.out.println("Good bye!"); break loop;
       default:
         System.out.println("지원하지 않는 명령어입니다.");
--- src/bitcamp/java89/ems/TeacherController.java
+++ src/bitcamp/java89/ems/TeacherController.java
@@ -3,77 +3,84 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class TeacherController {
-  static Teacher[] teachers = new Teacher[100];
-  static int length = 0;
-  static Scanner keyScan;
+  Teacher[] teachers = new Teacher[100];
+  int length = 0;
+  Scanner keyScan;
 
-  static void doView() {
+  //기본 생성자가 없다.
+  //따라서 이 클래스를 사용하려면 반드시 Scanner를 줘야 한다.
+  //=> 생성자에서 하는 일은 그 객체를 사용하기 전에 유효 상태로 만드는 것이다.
+  public TeacherController(Scanner keyScan) {
+    this.keyScan = keyScan;
+  }
+
+  public void doView() {
     int target = 0;
     //Scanner keyScan = new Scanner(System.in);
     System.out.print("강사 아이디? ");
-    String viewId = keyScan.nextLine().toLowerCase();
+    String viewId = this.keyScan.nextLine().toLowerCase();
 
-    for (int i = 0; i < length; i++) {
-      if (teachers[i].userId.toLowerCase().equals(viewId)) {
-        System.out.printf("아이디: %s\n", teachers[i].userId);
+    for (int i = 0; i < this.length; i++) {
+      if (this.teachers[i].userId.toLowerCase().equals(viewId)) {
+        System.out.printf("아이디: %s\n", this.teachers[i].userId);
         System.out.printf("암호: (***)\n");
-        System.out.printf("이름: %s\n", teachers[i].name);
-        System.out.printf("이메일: %s\n", teachers[i].email);
-        System.out.printf("전화: %s\n", teachers[i].tel);
-        System.out.printf("나이: %d\n", teachers[i].age);
-        System.out.printf("담당과목: %s\n", teachers[i].subject);
-        System.out.printf("경력: %d\n", teachers[i].carrer);
-        System.out.printf("연봉: %d\n", teachers[i].salary);
-        System.out.printf("주소: %s\n", teachers[i].address);
+        System.out.printf("이름: %s\n", this.teachers[i].name);
+        System.out.printf("이메일: %s\n", this.teachers[i].email);
+        System.out.printf("전화: %s\n", this.teachers[i].tel);
+        System.out.printf("나이: %d\n", this.teachers[i].age);
+        System.out.printf("담당과목: %s\n", this.teachers[i].subject);
+        System.out.printf("경력: %d\n", this.teachers[i].carrer);
+        System.out.printf("연봉: %d\n", this.teachers[i].salary);
+        System.out.printf("주소: %s\n", this.teachers[i].address);
         break;
       }
     }
   }
 
-  static void doAdd() {
+  public void doAdd() {
     Teacher teacher = new Teacher();
     //Scanner keyScan = new Scanner(System.in);
 
     System.out.print("아이디(예:hong)? ");
-    teacher.userId = keyScan.nextLine();
+    teacher.userId = this.keyScan.nextLine();
 
     System.out.print("암호(예:1234)? ");
-    teacher.password = keyScan.nextLine();
+    teacher.password = this.keyScan.nextLine();
 
     System.out.print("이름(예:홍길동)? ");
-    teacher.name = keyScan.nextLine();
+    teacher.name = this.keyScan.nextLine();
 
     System.out.print("이메일(예:hong@test.com)? ");
-    teacher.email = keyScan.nextLine();
+    teacher.email = this.keyScan.nextLine();
 
     System.out.print("전화(예:010-1111-2222)? ");
-    teacher.tel = keyScan.nextLine();
+    teacher.tel = this.keyScan.nextLine();
 
     System.out.print("나이(예:39)? ");
-    teacher.age = Integer.parseInt(keyScan.nextLine());
+    teacher.age = Integer.parseInt(this.keyScan.nextLine());
 
     System.out.print("담당과목(예:자바)? ");
-    teacher.subject = keyScan.nextLine();
+    teacher.subject = this.keyScan.nextLine();
 
     System.out.print("경력(예:10)? ");
-    teacher.carrer = Integer.parseInt(keyScan.nextLine());
+    teacher.carrer = Integer.parseInt(this.keyScan.nextLine());
 
     System.out.print("연봉(예:8500)? ");
-    teacher.salary = Integer.parseInt(keyScan.nextLine());
+    teacher.salary = Integer.parseInt(this.keyScan.nextLine());
 
     System.out.print("주소(예:서울시 서초구 서초동)? ");
-    teacher.address = keyScan.nextLine();
+    teacher.address = this.keyScan.nextLine();
 
-    teachers[length++] = teacher;
+    this.teachers[length++] = teacher;
   }
 
-  static void doList() {
+  public void doList() {
     if (length == 0) {
       System.out.println("리스트가 존재하지 않습니다.");
     } else {
       Teacher teacher;
-      for (int i = 0; i < length; i++) {
-        teacher = teachers[i];
+      for (int i = 0; i < this.length; i++) {
+        teacher = this.teachers[i];
         System.out.printf("%s, %s, %s, %s, %s, %d, %s, %d, %d, %s\n",
           teacher.userId,
           teacher.password,
